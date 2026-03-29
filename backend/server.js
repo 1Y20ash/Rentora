c28d4086b5f2c6116d1bb0c1b5f9f6957305f3ef
Yash Chitmalwar <yashchitmalwar@gmail.com>
2026-03-29 19:53:12 +0530

    Connected Railway DB

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,22 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ DB Connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // 🔥 ADD THIS
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
-db.connect(err => {
+db.getConnection((err, connection) => {
     if (err) {
-        console.log("DB Error:", err);
+        console.log("DB Connection Failed ❌", err);
     } else {
         console.log("Database Connected ✅");
+        connection.release();
     }
 });
 
